00d2093be784ae992c893f7ad1cd283736c91b5e
Brambs <gabriel128.br@gmail.com>
2023-07-12 14:30:24 -0300

    Deploy config

--- src/server.js
+++ src/server.js
@@ -71,7 +71,7 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3352
+const PORT = process.env.SERVER_PORT || 3333
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`)
